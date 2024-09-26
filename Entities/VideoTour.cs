6b8151e10b8be7eaef3e16103c3171341da25b58
jonne <jonnenc@gmail.com>
2024-09-26 19:54:50 -0400

    add video tour table

---
+++ Entities/VideoTour.cs
@@ -0,0 +1,33 @@
+namespace SimpleWebAppReact.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+/// <summary>
+/// 
+/// </summary>
+public class VideoTour
+{
+    [BsonId]
+    [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    
+    [BsonElement("title"), BsonRepresentation(BsonType.String)]
+    public string? Title { get; set; }
+    
+    [BsonElement("url"), BsonRepresentation(BsonType.String)]
+    public string? Url { get; set; }
+
+    [BsonElement("length"), BsonRepresentation(BsonType.Double)]
+    public Double? Length { get; set; }
+
+    [BsonElement("createdAt"), BsonRepresentation(BsonType.DateTime)]
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    [BsonElement("description"), BsonRepresentation(BsonType.String)]
+    public string? Description { get; set; }
+
+    [BsonElement("userId"), BsonRepresentation(BsonType.String)]
+    public string? UserId { get; set; }
+
+    [BsonElement("buildingId"), BsonRepresentation(BsonType.String)]
+    public string? BuildingId { get; set; }
+}
