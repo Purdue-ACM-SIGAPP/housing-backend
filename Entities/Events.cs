f81428d0b0501827dc02495a77509f6e2663135a
SashimiDaBest <soleilpham@gmail.com>
2024-09-26 19:54:06 -0400

    Added Fields to Events Object

---
+++ Entities/Events.cs
@@ -0,0 +1,26 @@
+namespace SimpleWebAppReact.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+/// <summary>
+/// Class structure matches 1-1 with Building Table in database
+/// </summary>
+public class Events
+{
+    [BsonElement("eventName"), BsonRepresentation(BsonType.String)]
+    public string? EventName { get; set; }
+    
+    [BsonElement("summary"), BsonRepresentation(BsonType.String)]
+    public string? Summary { get; set; }
+
+    [BsonElement("content"), BsonRepresentation(BsonType.String)]
+    public string? Content { get; set; }
+
+    [BsonElement("userID"), BsonRepresentation(BsonType.String)]
+    public string? UserID { get; set; }
+
+    [BsonElement("date"), BsonRepresentation(BsonType.DateTime)]
+    public DateTime? Date { get; set; }
+
+    [BsonElement("address"), BsonRepresentation(BsonType.String)]
+    public string? Address { get; set; }
+}
