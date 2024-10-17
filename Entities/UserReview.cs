e1184d9ff5e4e4f13ede23c56f8e4acdf5ceee6c
jonne <jonnenc@gmail.com>
2024-10-17 19:59:48 -0400

    add entity and controller files

---
+++ Entities/UserReview.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleWebAppReact.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+/// <summary>
+/// Class structure matches 1-1 with UserReview Table in database
+/// </summary>
+public class UserReview
+{
+    [BsonId]
+    [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    
+    [BsonElement("userId"), BsonRepresentation(BsonType.String)]
+    public string? UserId { get; set; }
+    
+    [BsonElement("reviewId"), BsonRepresentation(BsonType.String)]
+    public string? ReviewId { get; set; }
+}
