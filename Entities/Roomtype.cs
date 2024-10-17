8cf7e05a80fa8c5c5c44bf1e00be011fe5641496
bijang109 <bohyeon.j109@gmail.com>
2024-10-17 19:52:01 -0400

    Update Roomtype.cs
    Added Roomtype entities

--- Entities/Roomtype.cs
+++ Entities/Roomtype.cs
@@ -1,1 +1,28 @@
+namespace SimpleWebAppReact.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+/// <summary>
+/// Class structure matches 1-1 with Building Table in database
+/// </summary>
+public class Building
+{
+    [BsonId]
+    [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
 
+    [BsonElement("peoplePerBedroom"), BsonRepresentation(BsonType.Int32)]
+    public int? PeoplePerBedroom { get; set; }
+
+    [BsonElement("cost"), BsonRepresentation(BsonType.String)]
+    public string? Cost { get; set; }
+
+    [BsonElement("buildingId"), BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId? BuildingID { get; set; }
+
+    [BsonElement("description"), BsonRepresentation(BsonType.String)]
+    public string? Description { get; set; }
+
+    [BsonElement("images"), BsonRepresentation(BsonType.String)]
+    public string[]? Images { get; set; }
+
+}
