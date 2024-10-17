10a89ccca937cade30caf353e972348f74c43113
bijang109 <bohyeon.j109@gmail.com>
2024-10-17 19:52:38 -0400

    Update Roomtype.cs

--- Entities/Roomtype.cs
+++ Entities/Roomtype.cs
@@ -4,7 +4,7 @@ using MongoDB.Bson.Serialization.Attributes;
 /// <summary>
 /// Class structure matches 1-1 with Building Table in database
 /// </summary>
-public class Building
+public class Roomtype
 {
     [BsonId]
     [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
