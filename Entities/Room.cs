9d512b29780576f0de58a72f486d7ee38fc4ac3a
SashimiDaBest <soleilpham@gmail.com>
2024-11-19 20:05:31 -0500

    sub class housing

---
+++ Entities/Room.cs
@@ -0,0 +1,10 @@
+public class Room
+{
+    public string? Type { get; set; }  
+    public int Capacity { get; set; } 
+
+    public double HousingRate {get; set; }
+
+    public bool IsSharedBathroom {get; set; }
+
+}
